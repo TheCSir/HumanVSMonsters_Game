bdf07a7cfb0db019d8123c4fdfc2ba929b35145c
David Highet <s3025109@student.rmit.edu.au>
2019-03-23 22:13:41 +1100

    Added end turn button

--- src/main/java/boardgame/controller/MainController.java
+++ src/main/java/boardgame/controller/MainController.java
@@ -7,15 +7,13 @@ package boardgame.controller;
 import boardgame.view.HexagonTile;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.image.Image;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.ImagePattern;
 import javafx.scene.text.Text;
 import javafx.scene.transform.Translate;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -27,6 +25,8 @@ public class MainController implements Initializable {
     private final static double n = Math.sqrt(r * r * 0.75); // the inner radius from hexagon center to middle of the axis
     private final static double TILE_HEIGHT = 2 * r;
     private final static double TILE_WIDTH = 2 * n;
+    private static final String PLAYER1 = "Human Player";
+    private static final String PLAYER2 = "Monster Player";
 
     @FXML // fx:id="turnTime"
     private Text turnTime; // Value injected by FXMLLoader
@@ -37,12 +37,36 @@ public class MainController implements Initializable {
     @FXML
     private VBox tileInfoPane;
 
+
+    @FXML
+    private Text currentPlayer;
+
+    @FXML
+    private Button endTurnButton;
+
+
+
     double time = 60;
+
+    private String[] playerArray = {PLAYER1, PLAYER2};
+    private String activePlayer;
 
     //TODO move event.
 
     public MainController () {
+        activePlayer = playerArray[0];
+    }
 
+
+    //Temporary solution for testing of changing player. Not intended for submission.
+    private void changeActivePlayer() {
+        if (activePlayer.equals(playerArray[0])){
+            activePlayer = playerArray[1];
+            currentPlayer.setText(activePlayer);
+        }else {
+            activePlayer = playerArray[0];
+            currentPlayer.setText(activePlayer);
+        }
     }
 
     @Override
@@ -50,7 +74,7 @@ public class MainController implements Initializable {
         turnTime.setText("Turn Time " + time);
 
         drawBasicGrid(10, 10, r);
-
+        endTurnButton.setOnAction(e -> changeActivePlayer());
     }
 
 
---
+++ src/main/java/boardgame/gameModel/tiles/BasicTile.java
@@ -0,0 +1,10 @@
+package boardgame.gameModel.tiles;
+
+public class BasicTile implements ITile {
+
+    private Location location;
+
+    public BasicTile(Location l) {
+        this.location=l;
+    }
+}
